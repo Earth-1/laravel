c900aac3764113f94a23ea889d6f510061360491
Earth-1 <boonjing4@gmail.com>
2022-11-26 18:55:01 +0700

    first commit

---
+++ app/Http/Controllers/Admincontroller.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Catagory;
+
+class Admincontroller extends Controller
+{
+    public function view_catagory(){
+
+        $data=catagory::all();
+
+        return view ('admin.catagory',compact('data'));
+}
+public function add_catagory(Request $request){
+    $data = new catagory;
+    $data->catagory_name = $request->catagory;
+    $data->save();
+    return redirect()->back()->with('message', 'เพิ่มแล้ว');
+}
+
+public function delete_catagory($id){
+    $data=catagory::find($id);
+    $data->delete();
+    return redirect()->back()->with('message', 'ลบแล้ว');
+}
+}
