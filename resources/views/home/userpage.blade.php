76e7c2954eff8ddcbeabb2521c1c9e8fc0761b65
Earth-1 <boonjing4@gmail.com>
2022-11-29 23:31:30 +0700

    first commit

--- resources/views/home/userpage.blade.php
+++ resources/views/home/userpage.blade.php
@@ -51,13 +51,17 @@
       <!-- footer start -->
       @include('home.footer')
       <!-- footer end -->
-      <div class="cpy_">
+
+
+      {{-- <div class="cpy_">
          <p class="mx-auto">© 2021 All Rights Reserved By <a href="https://html.design/">Free Html Templates</a><br>
 
             Distributed By <a href="https://themewagon.com/" target="_blank">ThemeWagon</a>
 
          </p>
-      </div>
+      </div> --}}
+
+      
       <!-- jQery -->
       <script src="home/js/jquery-3.4.1.min.js"></script>
       <!-- popper js -->
