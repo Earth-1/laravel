db8731a4910f4255a447eb3251f843a3cdd7506e
Earth-1 <boonjing4@gmail.com>
2022-12-09 16:26:14 +0700

    17

--- app/Http/Controllers/Admincontroller.php
+++ app/Http/Controllers/Admincontroller.php
@@ -94,5 +94,13 @@ public function order(){
         $order = order::all();
         return view('admin.order',compact('order'));
 }
+public function delivered($id){
+        $order = order::find($id);
+        $order->delivery_status = "delivered";
+        $order->payment_status = "Paid";
+        $order->save();
+
+        return redirect()->back();
+}
 
 }
--- resources/views/admin/order.blade.php
+++ resources/views/admin/order.blade.php
@@ -45,16 +45,17 @@
                 <table class="tablecss"> 
                     <tr class="thcss">
 
-                        <th>Name</th>
-                        <th>Email</th>
-                        <th>Address</th>
-                        <th>Phone</th>
-                        <th>Product name</th>
-                        <th>Quantity</th>
-                        <th>Price</th>
-                        <th>Payment Status</th>
-                        <th>Delivery Status</th>
-                        <th>Image</th>
+                        <th style="padding: 10px">Name</th>
+                        <th style="padding: 10px">Email</th>
+                        <th style="padding: 10px">Address</th>
+                        <th style="padding: 10px">Phone</th>
+                        <th style="padding: 10px">Product name</th>
+                        <th style="padding: 10px">Quantity</th>
+                        <th style="padding: 10px">Price</th>
+                        <th style="padding: 10px">Payment Status</th>
+                        <th style="padding: 10px">Delivery Status</th>
+                        <th style="padding: 10px">Image</th>
+                        <th style="padding: 10px">Delivery</th>
 
                     </tr>
                     @foreach ($order as $order)
@@ -70,6 +71,23 @@
                         <td>{{$order->payment_status}}</td>
                         <td>{{$order->delivery_status}}</td>
                         <td><img src="/product/{{$order->image}}" class="imgcss"></td>
+                        <td>
+
+                            @if($order->delivery_status=='processing')
+
+                            <a 
+
+                            href="{{url('delivered',$order->id)}}" class="btn btn-primary" onclick="return confirm('คุณแน่ใจนะว่าจะส่งสินค้านี้')">Delivered
+
+                            </a>
+
+                            @else
+
+                            <p style="color: green;">Delivered</p>
+
+                            @endif
+
+                        </td>
                     </tr>
                     @endforeach
                 </table>
